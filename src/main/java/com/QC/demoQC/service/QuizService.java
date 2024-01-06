1dba1e36d737c7cb920c4e65fa87143fecf6d2ae
gopalbayas <149937126+gopalbayas@users.noreply.github.com>
2024-01-06 21:29:11 +0530

    Update QuizService.java

--- src/main/java/com/QC/demoQC/service/QuizService.java
+++ src/main/java/com/QC/demoQC/service/QuizService.java
@@ -1,5 +1,4 @@
 package com.QC.demoQC.service;
-
 import com.QC.demoQC.dao.QuestionDao;
 import com.QC.demoQC.dao.QuizDao;
 import com.QC.demoQC.model.Question;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,9 +27,9 @@ public class QuizService {
         quiz.setTitle(title);
         quiz.setQuestions(questions);
         quizDao.save(quiz);
-
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
+    
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
         Optional<Quiz> quiz = quizDao.findById(id);
         List<Question> questionsFromDB = quiz.get().getQuestions();
@@ -40,9 +38,7 @@ public class QuizService {
             QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
             questionsForUser.add(qw);
         }
-
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
-
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
@@ -53,7 +49,6 @@ public class QuizService {
         for(Response response : responses){
             if(response.getResponse().equals(questions.get(i).getRightAnswer()))
                 right++;
-
             i++;
         }
         return new ResponseEntity<>(right, HttpStatus.OK);
