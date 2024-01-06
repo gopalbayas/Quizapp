ad0b10691bef191a1f6992ff1f664966dd6d40e4
gopalbayas <149937126+gopalbayas@users.noreply.github.com>
2024-01-06 21:27:52 +0530

    Update QuestionDao.java

--- src/main/java/com/QC/demoQC/dao/QuestionDao.java
+++ src/main/java/com/QC/demoQC/dao/QuestionDao.java
@@ -1,18 +1,14 @@
 package com.QC.demoQC.dao;
-
 import com.QC.demoQC.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-
 public interface QuestionDao extends JpaRepository<Question,Integer> { // jpa will minimize 7 steps to fetch database
     //  data jpa  need 2 thing (tableName, PrimaryKey Type)
      List<Question> findByCategory(String category);
     @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
-
 }
